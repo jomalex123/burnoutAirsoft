d37b70a02296856362fb774933ed91bd82b0bf2a
Alex <jomalex123@gmail.com>
2026-05-01 23:15:06 +0200

    Fix env PRE

---
+++ config/env.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'default' => 'pre',
+    'connections' => [
+        'local' => [
+            'host' => '127.0.0.1',
+            'port' => 3306,
+            'database' => '11364681_burnoutairsoft',
+            'username' => 'boutAdmin',
+            'password' => 'Asdqwe123',
+            'charset' => 'utf8mb4',
+        ],
+        'pre' => [
+            'host' => 'PMYSQL201.dns-servicio.com',
+            'port' => 3306,
+            'database' => '11364681_burnoutairsoftPRE',
+            'username' => 'boutAdminPRE',
+            'password' => '$4XsLQ1gnr%w4cee',
+            'charset' => 'utf8mb4',
+        ],
+        'pro' => [
+            'host' => 'PMYSQL201.dns-servicio.com',
+            'port' => 3306,
+            'database' => '11364681_burnoutairsoft',
+            'username' => 'boutAdmin',
+            'password' => '#ko9365aH',
+            'charset' => 'utf8mb4',
+        ],
+    ],
+];
